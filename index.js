a1fcb2e6fb641b584c449ef6127c3faa69b9141d
Kohei Tahara <tahara.kohei@jp.panasonic.com>
2021-11-06 10:27:45 +0900

    v1.2.0  support json log format

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 const moment = require("moment-timezone");
+const stringify = require("json-stringify-safe");
 const LOG_LEVEL = ["trace", "debug", "info", "warn", "error", "fatal", "mark"];
 const LOG_LEVEL_INDEX = {};
 for (let i = 0; i < LOG_LEVEL.length; i++) {
   LOG_LEVEL_INDEX[LOG_LEVEL[i]] = i + 1;
 }
+
+const FORMAT_TYPE = {
+  TEXT: "text",
+  JSON: "json",
+};
 
 class Logger {
   get level() {
@@ -16,10 +22,19 @@ class Logger {
       LOG_LEVEL_INDEX[level.toLowerCase()] || LOG_LEVEL_INDEX["error"];
   }
 
-  constructor(level = "error", tag = "simple-logger", tz = "UTC", file = null) {
+  constructor(
+    level = "error",
+    tag = "simple-logger",
+    tz = "UTC",
+    file = null,
+    format = "text"
+  ) {
     this.level = level;
     this.tag = tag;
     this.tz = tz;
+    this.format = Object.values(FORMAT_TYPE).includes(format)
+      ? format
+      : FORMAT_TYPE.TEXT;
 
     if (file) {
       this.logFileStream = fs.createWriteStream(file, { flags: "a" });
@@ -28,12 +43,26 @@ class Logger {
     for (let level of LOG_LEVEL) {
       this[level] = (...msg) => {
         if (this.level > LOG_LEVEL_INDEX[level]) return;
-        const logMessage = `[${moment()
+        let logMessage = `[${moment()
           .tz(this.tz)
           .format("YYYY-MM-DDTHH:mm:ss.SSSZ")}][${level.toUpperCase()}][${
           this.tag
         }] ${[...msg].join(" ")}`;
 
+        if (this.format == FORMAT_TYPE.JSON) {
+          logMessage = stringify(
+            {
+              timestamp: moment()
+                .tz(this.tz)
+                .format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
+              logLevel: level.toUpperCase(),
+              logTag: this.tag,
+              logMessages: [...msg],
+            },
+            null,
+            "    "
+          );
+        }
         console.log(logMessage);
         if (this.logFileStream && this.logFileStream.writable) {
           this.logFileStream.write(logMessage + "\n");
@@ -42,8 +71,8 @@ class Logger {
     }
   }
 
-  static createLogger(logLevel, logTag, timezone, filepath) {
-    return new Logger(logLevel, logTag, timezone, filepath);
+  static createLogger(logLevel, logTag, timezone, filepath, format) {
+    return new Logger(logLevel, logTag, timezone, filepath, format);
   }
 }
 
